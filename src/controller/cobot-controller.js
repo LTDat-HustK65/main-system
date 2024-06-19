f645b41ce978da8ffa3d7c570ff4f3152406f465
LTDat-HustK65 <dat.lt203672@sis.hust.edu.vn>
2024-06-20 00:27:50 +0700

    new commit

--- src/controller/cobot-controller.js
+++ src/controller/cobot-controller.js
@@ -5,14 +5,169 @@ const RotateBaseRight = async (req, res) => {
     //method: POST
     try{
         const amplitude = req.body.amplitude;
-        req.io.emit('RotateBaseRight', { amplitude });
-        res.status(200).send({ message: 'Rotating base right...' });
+            req.io.emit('RotateBaseRight', amplitude);
+            res.status(200).send({
+                message: 'Rotating base right...' 
+            });
     }
     catch(error){
         res.status(400).send(error);
     }
 };
-
+const RotateBaseLeft = async (req, res) => {
+    //method: POST
+    try{
+        const amplitude = req.body.amplitude;
+            req.io.emit('RotateBaseLeft', amplitude);
+            res.status(200).send({
+                message: 'Rotating base left...' 
+            });
+    }
+    catch(error){
+        res.status(400).send(error);
+    }
+};
+const RotateServo01Up = async (req, res) => {
+    //method: POST
+    try{
+        const amplitude = req.body.amplitude;
+            req.io.emit('RotateServo01Up', amplitude);
+            res.status(200).send({
+                message: 'Rotating servo 01 up...' 
+            });
+    }
+    catch(error){
+        res.status(400).send(error);
+    }
+};
+const RotateServo01Down = async (req, res) => {
+    //method: POST
+    try{
+        const amplitude = req.body.amplitude;
+            req.io.emit('RotateServo01Down', amplitude);
+            res.status(200).send({
+                message: 'Rotating servo 01 down...' 
+            });
+    }
+    catch(error){
+        res.status(400).send(error);
+    }
+}
+const RotateServo02Up = async (req, res) => {
+    //method: POST
+    try{
+        const amplitude = req.body.amplitude;
+            req.io.emit('RotateServo02Up', amplitude);
+            res.status(200).send({
+                message: 'Rotating servo 02 up...' 
+            });
+    }
+    catch(error){
+        res.status(400).send(error);
+    }
+}
+const RotateServo02Down = async (req, res) => {
+    //method: POST
+    try{
+        const amplitude = req.body.amplitude;
+            req.io.emit('RotateServo02Down', amplitude);
+            res.status(200).send({
+                message: 'Rotating servo 02 down...' 
+            });
+    }
+    catch(error){
+        res.status(400).send(error);
+    }
+}
+const RotateClawRight = async (req, res) => {
+    //method: POST
+    try{
+        const amplitude = req.body.amplitude;
+            req.io.emit('RotateClawRight', amplitude);
+            res.status(200).send({
+                message: 'Rotating claw right...' 
+            });
+    }
+    catch(error){
+        res.status(400).send(error);
+    }
+}
+const RotateClawLeft = async (req, res) => {
+    //method: POST
+    try{
+        const amplitude = req.body.amplitude;
+            req.io.emit('RotateClawLeft', amplitude);
+            res.status(200).send({
+                message: 'Rotating claw left...' 
+            });
+    }
+    catch(error){
+        res.status(400).send(error);
+    }
+}
+const RotateClawUp = async (req, res) => {
+    //method: POST
+    try{
+        const amplitude = req.body.amplitude;
+            req.io.emit('RotateClawUp', amplitude);
+            res.status(200).send({
+                message: 'Rotating claw up...' 
+            });
+    }
+    catch(error){
+        res.status(400).send(error);
+    }
+}
+const RotateClawDown = async (req, res) => {
+    //method: POST
+    try{
+        const amplitude = req.body.amplitude;
+            req.io.emit('RotateClawDown', amplitude);
+            res.status(200).send({
+                message: 'Rotating claw down...' 
+            });
+    }
+    catch(error){
+        res.status(400).send(error);
+    }
+}
+const RotateClaw = async (req, res) => {
+    //method: POST
+    try{
+        const amplitude = req.body.amplitude;
+            req.io.emit('RotateClaw', amplitude);
+            res.status(200).send({
+                message: 'Rotating claw close...' 
+            });
+    }
+    catch(error){
+        res.status(400).send(error);
+    }
+}
+const ReleaseClaw = async (req, res) => {
+    //method: POST
+    try{
+        const amplitude = req.body.amplitude;
+            req.io.emit('ReleaseClaw', amplitude);
+            res.status(200).send({
+                message: 'Releasing claw...' 
+            });
+    }
+    catch(error){
+        res.status(400).send(error);
+    }
+}
 module.exports = {
-    RotateBaseRight
+    RotateBaseRight,
+    RotateBaseLeft,
+    RotateServo01Up,
+    RotateServo01Down,
+    RotateServo02Up,
+    RotateServo02Down,
+    RotateClawRight,
+    RotateClawLeft,
+    RotateClawUp,
+    RotateClawDown,
+    RotateClaw,
+    ReleaseClaw
 };
--- src/routes/cobot-api.js
+++ src/routes/cobot-api.js
@@ -1,15 +1,36 @@
 const cors = require('cors');
 const Cobot_api = require('express').Router();
-const { 
-    RotateBaseRight
+const {
+    RotateBaseRight,
+    RotateBaseLeft,
+    RotateServo01Up,
+    RotateServo01Down,
+    RotateServo02Up,
+    RotateServo02Down,
+    RotateClawRight,
+    RotateClawLeft,
+    RotateClawUp,
+    RotateClawDown,
+    RotateClaw,
+    ReleaseClaw
 } = require('../controller/cobot-controller');
 
 Cobot_api.use(cors());
 
 // Các api để điểu khiển cobot
 //http://localhost:3030/cobot_api
-
 Cobot_api.post('/rotate_base_right', RotateBaseRight);
+Cobot_api.post('/rotate_base_left', RotateBaseLeft);
+Cobot_api.post('/rotate_servo_01_up', RotateServo01Up);
+Cobot_api.post('/rotate_servo_01_down', RotateServo01Down);
+Cobot_api.post('/rotate_servo_02_up', RotateServo02Up);
+Cobot_api.post('/rotate_servo_02_down', RotateServo02Down);
+Cobot_api.post('/rotate_claw_right', RotateClawRight);
+Cobot_api.post('/rotate_claw_left', RotateClawLeft);
+Cobot_api.post('/rotate_claw_up', RotateClawUp);
+Cobot_api.post('/rotate_claw_down', RotateClawDown);
+Cobot_api.post('/rotate_claw', RotateClaw);
+Cobot_api.post('/release_claw', ReleaseClaw);
 
 
 module.exports = Cobot_api; 
